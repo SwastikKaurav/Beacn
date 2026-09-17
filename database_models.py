a98b02bb4cbbbb084af258561df9cc08c9422e7f
SwastikKaurav <kauravswastik15@gmail.com>
2026-09-17 18:45:25 +0530

    added user table and user_id column in endpoints table, also added pydantic validation for user input and user response

--- database_models.py
+++ database_models.py
@@ -1,14 +1,25 @@
 from database import Base
 from sqlalchemy import Column, String, Integer, Float, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
+from datetime import datetime
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    email = Column(String, unique=True)
+    hashed_password = Column(String)
+    created_at = Column(DateTime, default=datetime.utcnow)
+    endpoints = relationship("Endpoint", back_populates="user")
 
 class Endpoint(Base):
     __tablename__ = "endpoints"
     id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey(User.id), nullable=False)
     name = Column(String)
     url = Column(String)
     ping_interval = Column(Integer)
     ping_results = relationship("PingResult", back_populates="endpoint")
+    user = relationship("User", back_populates="endpoints")
 
 class PingResult(Base):
     __tablename__ = "ping_results"
@@ -18,3 +29,4 @@ class PingResult(Base):
     response_time = Column(Float)
     checked_at = Column(DateTime)
     endpoint = relationship("Endpoint", back_populates="ping_results")
+
