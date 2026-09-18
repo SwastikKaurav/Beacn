d2c2c794e5f762e2eff65b52aa05382b14b5cb64
SwastikKaurav <kauravswastik15@gmail.com>
2026-09-18 12:02:25 +0530

    auth functions completed

--- routers/auth.py
+++ routers/auth.py
@@ -5,6 +5,11 @@ from jwt.exceptions import ExpiredSignatureError, InvalidTokenError
 from dotenv import load_dotenv
 import os
 from datetime import datetime, timezone, timedelta
+from sqlalchemy.orm import Session
+from database import get_db
+from fastapi.security import OAuth2PasswordBearer
+from fastapi import Depends, HTTPException
+from database_models import User
 
 ph = PasswordHasher()
 
@@ -36,3 +41,21 @@ def verify_access_token(token: str):
             algorithms=["HS256"]
         )
         return payload
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
+
+def get_current_user(token : str = Depends(oauth2_scheme), db : Session = Depends(get_db)):
+    try:
+        payload = verify_access_token(token)
+
+    except ExpiredSignatureError :
+        raise HTTPException(status_code=401, detail="Token expired")
+    except InvalidTokenError :
+         raise HTTPException(status_code=401, detail="Invalid Token")
+
+    user_id = payload["user_id"]
+    user = db.query(User).filter(User.id == user_id).first()
+    if user is None:
+        raise HTTPException(status_code=401, detail="No user found")
+    return user
+     
